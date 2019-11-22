d18b873c71bc535cb5303709ef17f1899ae67975
Isabel Kain <kain.i@husky.neu.edu>
2019-11-22 17:33:42 +0100

    Added nav bar, second page for manipulating config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <Navigation></Navigation>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'Navigation': Navigation
+  }
 }
 </script>
 
@@ -21,3 +27,24 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<!--style lang="scss">
+  $navbar-background-color: $blue;
+  $navbar-item-color : $white;
+</style-->
+
+<!-- Lifted from H4WEBGUI-->
+<!--style lang="scss">
+// Import Bulma's core
+// @import 'bulma';
+@import "~bulma/sass/utilities/_all";
+@import '~bulma-checkradio';
+
+// $box-radius: 0;
+// $section-padding: 1.5rem 1rem;
+$navbar-background-color: $blue;
+$navbar-item-color : $white;
+
+// Import Bulma and Buefy styles
+@import "bulma";
+</style-->
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template> <!-- could use CSS grid-->
   <div class="hello">
     <title>VICE++ Control GUI</title>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <!-- <h1>{{ msg }}</h1> -->
+    <!-- <h2>Essential Links</h2>
     <ul>
       <li>
         <a
@@ -80,42 +80,60 @@
           awesome-vue
         </a>
       </li>
-    </ul>
-    <section style="background-color:black;color:white;margin:10px 5px 10px 5px;padding:10px 5px 10px 5px;"> <!-- Area for FSM status -- align left-->
-      FSM to be built here
-    </section>
-    <section style="background-color:#e6f2ff;color:black;margin:10px 5px 10px 5px;padding:10px 5px 10px 5px;"> <!-- Area for configurables -- align right-->
-      Configurables to be placed here
-      <section>
-            <form id="numCards">
-              <p><label>Number of VFEs: <input form="numCards" type="number" id="numVFE" maxlength="5" style="width:120px;"></label></p>
-              <p><label>Number of FEs: <input form="numCards" type="number" id="numFE" maxlength="5" style="width:120px;"></label></p>
-            </form>
+    </ul> -->
+    <!-- <Navigation name="nav"></Navigation> -->
+    <div class="row">
+      <div class="column is-half">
+        <div class="fsm">
+          <section style="background-color:black;color:white;margin:10px 5px 10px 5px;padding:10px 5px 10px 5px;"> <!-- Area for FSM status -- align left-->
+            FSM div to be built here
+            <div class="columns is-0">
+              <div class="Initialized">Initialized</div>
+              <div class="Configured">Configured</div>
+              <div class="Running">Running</div>
+              <div class="Paused">Paused</div>
+              <div class="Error">Error</div>
+            </div>
+          </section>
+        </div>
+      </div>
 
-            <!-- <button id='clickMe' v-on:click="contactServer()">Click me!</button><br> -->
+      <div class="column is-half">
+        <div class="config">
+          <section style="background-color:#e6f2ff;color:black;margin:10px 5px 10px 5px;padding:10px 5px 10px 5px;"> <!-- Area for configurables -- align right-->
+            Configurables to be placed here
+            <section>
+                  <form id="numCards">
+                    <p><label>Number of VFEs: <input form="numCards" type="number" id="numVFE" maxlength="5" style="width:120px;"></label></p>
+                    <p><label>Number of FEs: <input form="numCards" type="number" id="numFE" maxlength="5" style="width:120px;"></label></p>
+                  </form>
 
-            <form>
-              <input type="radio" name='clock' id='clockSource' value="1">Clock Source 1<br>
-              <input type="radio" name='clock' id='clockSource' value="2">Clock Source 2<br>
-            </form><br>
+                  <!-- <button id='clickMe' v-on:click="contactServer()">Click me!</button><br> -->
 
-            <select id='dropSelect'>
-              <option selected disabled>Choose one</option>
-              <option value="a">A</option>
-              <option value="b">B</option>
-              <option value="c">C</option>
-              <option value="d">D</option>
-            </select>
+                  <form>
+                    <input type="radio" name='clock' id='clockSource' value="1">Clock Source 1<br>
+                    <input type="radio" name='clock' id='clockSource' value="2">Clock Source 2<br>
+                  </form><br>
 
-            <p></p>
+                  <select id='dropSelect'>
+                    <option selected disabled>Choose one</option>
+                    <option value="a">A</option>
+                    <option value="b">B</option>
+                    <option value="c">C</option>
+                    <option value="d">D</option>
+                  </select>
 
-            <button v-on:click="readOptions()">Show selections</button>
-            <button v-on:click="submitChanges()">Update from selection</button>
+                  <p></p>
 
-      </section>
-    </section>
+                  <button v-on:click="readOptions()">Show selections</button>
+                  <button v-on:click="submitChanges()">Update from selection</button>
+
+            </section>
+          </section>
+        </div>
+      </div>
+    </div>
   </div>
-  <!-- <div></div> -->
 </template>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 <script>
@@ -181,5 +199,17 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.column {
+  float: left;
+  width: 50%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
 }
 </style>
